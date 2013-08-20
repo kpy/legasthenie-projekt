33b856e58be0b7d98a65d5ea42e83fa8d35f456d
SWei <kpy00@web.de>
2013-08-20 18:58:45 +0200

    Repository initalisiert

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	// index.php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Legasthenie-Projekt</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
+</head>
+<body>
+	<div id="mainwrapper">
+		<h1>Legasthenie-Projekt</h1>
+	</div>
+</body>
+</html>
